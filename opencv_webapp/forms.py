a4b63e5a46c651b257023a233cca5332a4f9d24e
cjfgml123 <cjfgml2358@naver.com>
2020-02-17 16:15:28 +0900

    django opencv

---
+++ opencv_webapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from  .models import ImageUploadModel
+
+class SimpleUploadForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    image = forms.ImageField()
+
+# 1) Hard-coded HTML : form, input, label, button,...
+# 2) forms.Form : 양식(Form class) 안에 받아들일 Field 선언
+
+class ImageUploadForm(forms.ModelForm):
+# Form을 통해 받아들여야 할 데이터가 명시되어 있는 메타 데이터 (DB 테이블을 연결)
+    class Meta:
+        model = ImageUploadModel
+        # Form을 통해 사용자로부터 입력 받으려는 Model Class의 field 리스트
+        fields = ('description', 'document',)
